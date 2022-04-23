9ef79b9d5b949c3766c353620707067b9c151411
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-04-23 19:59:57 +0530

    init

---
+++ contracts/Miscellaneous/CompoundMappingInterface.sol
@@ -0,0 +1,39 @@
+//Sample to understand arguements of InstaCompoundMapping contract 
+//It's address is used in main Compound.sol
+
+// Normal Token
+// ETH : 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
+// BAT : 0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99
+// DAI : 0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa
+// USDC : 0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b
+// USDT : 0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02
+// wBTC : 0x577D296678535e4903D59A4C929B718e1D575e0A
+
+
+// Compound Tokens
+// cETH : 0xd6801a1dffcd0a410336ef88def4320d6df1883e
+// cBAT : 0xebf1a11532b93a529b5bc942b4baa98647913002
+// cDAI : 0x6d7f0754ffeb405d23c51ce938289d4835be3b14
+// cUSDC : 0x5b281a6dda0b271e91ae35de655ad301c976edb1
+// cUSDT : 0x2fb298bdbef468638ad6653ff8376575ea41e768
+// wBTC : 0x0014f450b8ae7708593f4a46f8fa6e5d50620f96 	
+
+
+Contract InstaCompoundMapping
+
+_CONNECTORS : 0x1802f70d911a6B74e6c55AE634BB5fdca1B1309a (Rinkeby)
+_CTOKENNAMES : ["ETH","BAT","DAI","USDC","USDT","wBTC"]
+
+_TOKENS : ["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+            "0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99",
+            "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
+            "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b",
+            "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02",
+            "0x577D296678535e4903D59A4C929B718e1D575e0A"]
+
+_CTOKENS : ["0xd6801a1dffcd0a410336ef88def4320d6df1883e",
+            "0xebf1a11532b93a529b5bc942b4baa98647913002",
+            "0x6d7f0754ffeb405d23c51ce938289d4835be3b14",
+            "0x5b281a6dda0b271e91ae35de655ad301c976edb1",
+            "0x2fb298bdbef468638ad6653ff8376575ea41e768",
+            "0x0014f450b8ae7708593f4a46f8fa6e5d50620f96"]
